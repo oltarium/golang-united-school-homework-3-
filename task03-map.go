9a111c3bd6427bb9004d223b5d267fbbce058cc0
Gleb Kazak <gleb.kazak@ukg.com>
2022-05-08 16:21:58 +0300

    task-03

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,15 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	var keys []int
+	for key, _ := range input {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
+		result = append(result, input[key])
+	}
 	return
 }
