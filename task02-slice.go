e501ece0067d3f80fdd3929e28404d040da10465
Gleb Kazak <gleb.kazak@ukg.com>
2022-05-11 19:07:16 +0300

    task-3: update

--- task02-slice.go
+++ task02-slice.go
@@ -1,7 +1,7 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	for i := len(input) - 1; i > 0; i-- {
+	for i := len(input) - 1; i >= 0; i-- {
 		result = append(result, input[i])
 	}
 	return
